000d9cec9fd0f599f62824ce2c423a72245ea3c0
yjiahao <yjiahao123@gmail.com>
2026-01-16 09:52:13 +0800

    add javadoc for public methods

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -2,28 +2,32 @@ package main.java;
 
 public class Echo {
 
-    private static String GREETING = "Hello! I'm Echo\n" + "What can I do for you?";
-    private static String EXIT_MESSAGE = "Bye. Hope to see you again soon!";
-    private static String SEPARATOR = "____________________________________________________________";
+    private static String greeting = "Hello! I'm Echo\n" + "What can I do for you?";
+    private static String exitMessage = "Bye. Hope to see you again soon!";
+    private static String separator = "____________________________________________________________";
 
     public Echo() {
 
     }
 
     private String greetUser() {
-        return Echo.GREETING;
+        return Echo.greeting;
     }
 
     private String exitUser() {
-        return Echo.EXIT_MESSAGE;
+        return Echo.exitMessage;
     }
 
+    /**
+     * Greets the user and exits immediately after
+     * @return Greeting and exit message before exiting
+     */
     public String greetAndExit() {
         String greeting = this.greetUser();
         String exitString = this.exitUser();
 
-        String greetingAndExit = Echo.SEPARATOR + "\n" + greeting + "\n" +
-            Echo.SEPARATOR + "\n" + exitString + "\n" + Echo.SEPARATOR;
+        String greetingAndExit = Echo.separator + "\n" + greeting + "\n" +
+            Echo.separator + "\n" + exitString + "\n" + Echo.separator;
         return greetingAndExit;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
 package main.java;
 
-import main.java.Echo;
-
+/**
+ * Main entry point for the chatbot.
+ * This class initializes and runs the Echo program which greets the user
+ * and displays an exit message.
+ */
 public class Main {
+    /**
+     * Main method that starts the chatbot.
+     * Creates an Echo instance and prints the greeting and exit messages.
+     *
+     * @param args Command line arguments (not used)
+     */
     public static void main(String[] args) {
         Echo echo = new Echo();
         System.out.println(echo.greetAndExit());
