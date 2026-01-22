80a0a3bc301df69c53e173b1116c001920d38027
yjiahao <yjiahao123@gmail.com>
2026-01-22 17:42:53 +0800

    refactor: Add Ui class to deal with formatting bot messages

---
+++ src/main/java/Ui.java
@@ -0,0 +1,87 @@
+package main.java;
+
+import java.util.ArrayList;
+
+public class Ui {
+    private static final String GREETING = "Hello! I'm Echo\n" + "What can I do for you?";
+    private static final String EXIT_MESSAGE = "Bye. Hope to see you again soon!";
+    private static final String SEPARATOR = "____________________________________________________________";
+
+    public Ui() {
+
+    }
+
+    /**
+     * Greets user with a GREETING.
+     * @return a GREETING to the user of type string.
+     */
+    public String greetUser() {
+        return Ui.SEPARATOR + "\n" + Ui.GREETING + "\n" + Ui.SEPARATOR;
+    }
+
+    /**
+     * Says bye to the user.
+     * @return an ending message for exiting the chatbot.
+     */
+    public String exitUser() {
+        return Ui.SEPARATOR + "\n" + Ui.EXIT_MESSAGE + "\n" + Ui.SEPARATOR + "\n";
+    }
+
+    /**
+     * Creates a message for the user after adding a task.
+     * @param taskString String representation of the task that has been added
+     * @param numTasks Number of tasks remaining
+     * @return description to inform user the addition of a new task.
+     */
+    public String createAddTaskMessage(Task task, int numTasks) {
+        return Ui.SEPARATOR + "\n" + "Got it. I've added this task:\n  "
+            + task.toString() + "\n" + "Now you have " + numTasks + " tasks in the list."
+                + "\n" + Ui.SEPARATOR;
+    }
+
+    /**
+     * Formats the tasks for the user to see in the user interface.
+     * @param tasks ArrayList of Task.
+     * @return Formatted string of Tasks suitable for the user interface.
+     */
+    public String createListTaskMessage(ArrayList<Task> tasks) {
+        String res = "";
+        int i = 1;
+        for (Task t : tasks) {
+            res = res + i + ". " + t + "\n";
+            i += 1;
+        }
+        String tasksString = res.stripTrailing();
+        return Ui.SEPARATOR + "\n" + "Here are the tasks in your list:\n"
+            + "\n" + tasksString + "\n" + Ui.SEPARATOR;
+    }
+
+    /**
+     * Generates a message that tells user task is marked as done.
+     * @param task Task that has been marked as done.
+     * @return String of formatted message for the user.
+     */
+    public String createMarkAsDoneMessage(Task task) {
+        return Ui.SEPARATOR + "\n" + "Nice! I've marked this task as done:\n  "
+            + task.toString() + "\n" + Ui.SEPARATOR;
+    }
+
+    /**
+     * Generates a message that tells user task is marked as not done yet.
+     * @param task Task that has been marked as undone.
+     * @return String of formatted message for the user.
+     */
+    public String createMarkAsUndoneMessage(Task task) {
+        return Ui.SEPARATOR + "\n" + "OK, I've marked this task as not done yet:\n  "
+            + task.toString() + "\n" + Ui.SEPARATOR;
+    }
+
+    public String createRemoveTaskMessage(Task task, int numTasks) {
+        return Ui.SEPARATOR + "\n" + "Noted. I've removed this task:\n  "
+            + task.toString() + "\n" + "Now you have " + numTasks + " tasks in the list."
+                + "\n" + Ui.SEPARATOR;
+    }
+}
+
+// class deals with interactions with the user
+// should be able to read and write from the user inputs
