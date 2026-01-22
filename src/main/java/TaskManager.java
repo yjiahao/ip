562a948b01775f8b7d60537e6fc7e05e70add7f2
yjiahao <yjiahao123@gmail.com>
2026-01-22 17:41:46 +0800

    refactor: Change return types of TaskManager methods

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -20,19 +20,19 @@ public class TaskManager {
      * Adds a new task to the task manager.
      * @param taskDescription description of task to be added.
      */
-    public String addTask(String taskDescription, Command type, ArrayList<String> commandArgs) {
+    public Task addTask(String taskDescription, Command type, ArrayList<String> commandArgs) {
         if (type.equals(Command.EVENT)) {
             Task event = new Event(taskDescription, commandArgs.get(0), commandArgs.get(1));
             this.tasks.add(event);
-            return event.toString();
+            return event;
         } else if (type.equals(Command.TODO)) {
             Task todo = new ToDo(taskDescription);
             this.tasks.add(todo);
-            return todo.toString();
+            return todo;
         } else {
             Task deadline = new Deadline(taskDescription, commandArgs.get(0));
             this.tasks.add(deadline);
-            return deadline.toString();
+            return deadline;
         }
     }
 
@@ -49,24 +49,18 @@ public class TaskManager {
      * @param taskNumber the 1-indexed Task that is to be removed.
      * @return the Task that was removed.
      */
-    public String removeTask(int taskNumber) {
+    public Task removeTask(int taskNumber) {
         this.checkNotOutOfBounds(taskNumber);
         Task removedTask = this.tasks.remove(taskNumber - 1);
-        return removedTask.toString();
+        return removedTask;
     }
 
     /**
-     * Get all tasks in the task manager and format them into a string.
-     * @return a string of numbered tasks, separated by newlines.
+     * Get all tasks in the task manager as an ArrayList of Task.
+     * @return a string of tasks.
      */
-    public String getTasks() {
-        String res = "";
-        int i = 1;
-        for (Task t : this.tasks) {
-            res = res + i + ". " + t + "\n";
-            i += 1;
-        }
-        return res.stripTrailing();
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
     }
 
     /**
@@ -74,24 +68,24 @@ public class TaskManager {
      * @param taskNumber the task number (1-indexed) to mark as done.
      * @return the task that was marked as done in String.
      */
-    public String markAsDone(int taskNumber) {
+    public Task markAsDone(int taskNumber) {
         this.checkNotOutOfBounds(taskNumber);
         // array is 0 indexed so need to translate by 1
         Task t = this.tasks.get(taskNumber - 1);
         t.markAsDone();
-        return t.toString();
+        return t;
     }
 
     /**
-     * Marks a task as undone.
+     * Marks a Task as undone.
      * @param taskNumber the task number (1-indexed) to mark as undone.
-     * @return the task that was marked as undone in String.
+     * @return the Task that was marked as undone.
      */
-    public String markAsUndone(int taskNumber) {
+    public Task markAsUndone(int taskNumber) {
         this.checkNotOutOfBounds(taskNumber);
         Task t = this.tasks.get(taskNumber - 1);
         t.markAsUndone();
-        return t.toString();
+        return t;
     }
 
     public int getNumTasks() {
