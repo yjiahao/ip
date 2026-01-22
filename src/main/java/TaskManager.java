4a99dc7fd909ea7530a9182800e113479e0c914a
yjiahao <yjiahao123@gmail.com>
2026-01-22 13:23:00 +0800

    feature: Add persistent task saving for TaskManager
    Add methods to TaskManager to allow saving and loading of tasks into a
    .txt file, currently hardcoded

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,8 +1,15 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskManager {
+
+    private static final String FILE_PATH = "data/echo.txt";
     private ArrayList<Task> tasks;
 
     public TaskManager() {
@@ -89,5 +96,87 @@ public class TaskManager {
 
     public int getNumTasks() {
         return this.tasks.size();
+    }
+
+    /**
+     * Save the tasks currently present in the tasks ArrayList
+     * @throws IOException
+     */
+    public void saveTasks() throws IOException {
+        File file = new File(TaskManager.FILE_PATH);
+        // clear contents of file first
+        FileWriter clearWriter = new FileWriter(file, false);
+        clearWriter.close();
+
+        // append to the empty file
+        FileWriter writer = new FileWriter(file, true);
+        for (Task task : this.tasks) {
+            String saveString = task.saveRepresentation();
+            // save line with newline
+            writer.write(saveString + System.lineSeparator());
+        }
+        writer.close();
+        return;
+    }
+
+    /**
+     * Load the tasks from a text file if it exists, else creates new empty file
+     * @throws IOException, TaskManagerException
+     */
+    public void loadTasks() throws FileNotFoundException, TaskManagerException {
+        File file = new File(TaskManager.FILE_PATH);
+        boolean canRead = file.canRead();
+        if (canRead) {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                // parse the line
+                Task task = this.parseSavedTask(line);
+                this.tasks.add(task);
+            }
+            scanner.close();
+        }
+        return;
+    }
+
+    /**
+     * Read each line in String and parses it to return a new Task
+     * Lines that are expected from the file:
+     * ToDo: T | 1 | read book
+     * Deadline: D | 0 | return book | June 6th
+     * Event: E | 1 | project meeting | Aug 6th 2pm | 4pm
+     * @param line String of a task that is currently in the File
+     * @return a new Task that has been created after successful parsing and creation
+     * @throws TaskManagerException when an invalid task type is detected
+     */
+    // TODO: handle this exception in the Main
+    // TODO: we can check the length of args also to check the validity of each line
+    private Task parseSavedTask(String line) throws TaskManagerException {
+        String[] args = line.split(" \\| ");
+        // check first arg to determine task type
+        if (args[0].equals("T")) {
+            // create new ToDo
+            Task todo = new ToDo(args[2]);
+            if (args[1].equals("1")) {
+                todo.markAsDone();
+            }
+            return todo;
+        } else if (args[0].equals("D")) {
+            // create new Deadline
+            Task deadline = new Deadline(args[2], args[3]);
+            if (args[1].equals("1")) {
+                deadline.markAsDone();
+            }
+            return deadline;
+        } else if (args[0].equals("E")) {
+            // create new Event
+            Task event = new Event(args[2], args[3], args[4]);
+            if (args[1].equals("1")) {
+                event.markAsDone();
+            }
+            return event;
+        } else {
+            throw new TaskManagerException("Invalid task type when parsing!");
+        }
     }
 }
