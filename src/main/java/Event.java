d73623ded68f330a28cd37b270590ea8c62c1ad5
yjiahao <yjiahao123@gmail.com>
2026-01-22 12:52:00 +0800

    feature: Allow Task and children of Task to save string representation

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -20,4 +20,15 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + this.start + " to: " + this.end + ")";
     }
+
+    /**
+     * Create String representation of Event task.
+     * @return String representation of the Event task ready to be saved into a .txt file.
+     * Return String is of the form: E | 1 | project meeting | Aug 6th 2pm | 4pm
+     */
+    @Override
+    public String saveRepresentation() {
+        int isDone = super.isDone ? 1 : 0;
+        return "D | " + isDone + " | " + super.description + " | " + this.start + " | " + this.end;
+    }
 }
