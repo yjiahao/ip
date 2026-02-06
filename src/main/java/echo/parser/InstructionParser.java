cb5114bbaddf2a29357d8a551e5c2c0e6d298c56
yjiahao <yjiahao123@gmail.com>
2026-02-06 16:00:31 +0800

    refactor: Abstract repeated code into private functions in parseCommand
    parseCommand method has a lot of repeated checks that raise exceptions.
    This reduces the readability and maintainability of the code.
    
    Refactor common exception checks for invalid user commands into private
    methods.

--- src/main/java/echo/parser/InstructionParser.java
+++ src/main/java/echo/parser/InstructionParser.java
@@ -34,71 +34,48 @@ public class InstructionParser {
         String[] parts = trimmedCommand.split(" ", 2);
         // get the keyword from user which is the first element
         String keyword = parts[0].toLowerCase();
+
         switch (keyword) {
         case "todo":
-            if (parts.length < 2) {
-                throw new ParsingException("The todo description cannot be empty leh...");
-            }
+            checkLengthMoreThanEqualTwo(parts, "The todo description cannot be empty leh...");
             return Command.TODO;
         case "deadline":
-            if (parts.length < 2) {
-                throw new ParsingException("got no deadline description and by when you should complete it...");
-            }
+            checkLengthMoreThanEqualTwo(parts, "got no deadline description and by when you should complete it...");
             // catch no description of deadline
             if (parts[1].startsWith("/by")) {
                 throw new ParsingException("got no deadline description...");
             }
             // catch no /by ... for deadline
             String[] deadlineParts = parts[1].split("/by ");
-            if (deadlineParts.length < 2) {
-                throw new ParsingException("need to indicate when you need to complete the deadline by!");
-            }
+            checkLengthMoreThanEqualTwo(deadlineParts, "need to indicate when you need to complete the deadline by!");
             return Command.DEADLINE;
         case "list":
             return Command.LIST;
         case "event":
-            if (parts.length < 2) {
-                throw new ParsingException("why got no event description one...");
-            }
+            checkLengthMoreThanEqualTwo(parts, "why got no event description one...");
             return Command.EVENT;
         case "mark":
-            if (parts.length < 2) {
-                throw new ParsingException("mark requires a task number!");
-            }
+            checkLengthMoreThanEqualTwo(parts, "mark requires a task number!");
             // catch cases like "mark string" instead of "mark 1"
-            try {
-                int num = Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                throw new ParsingException("mark needs a valid task number!");
-            }
+            checkValidNumber(parts, "mark needs a valid task number!");
             return Command.MARK;
         case "unmark":
-            if (parts.length < 2) {
-                throw new ParsingException("unmark requires a task number!");
-            }
+            checkLengthMoreThanEqualTwo(parts, "unmark requires a task number!");
             // catch cases like "unmark string" instead of "unmark 1"
-            try {
-                int num = Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                throw new ParsingException("unmark needs a valid task number!");
-            }
+            checkValidNumber(parts, "unmark needs a valid task number!");
             return Command.UNMARK;
         case "delete":
-            if (parts.length < 2) {
-                throw new ParsingException("delete requires a task number!");
-            }
+            checkLengthMoreThanEqualTwo(parts, "delete requires a task number!");
             // catch cases like "delete string" instead of "delete 1"
-            try {
-                int num = Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                throw new ParsingException("delete needs a valid task number!");
-            }
+            checkValidNumber(parts, "delete needs a valid task number!");
             return Command.DELETE;
         case "bye":
             return Command.BYE;
         case "find":
+            checkLengthMoreThanEqualTwo(parts, "find requires a keyword!");
             return Command.FIND;
         default:
+            // unknown command expected here
             throw new ParsingException("Sorry what does that mean ah? I never see " + keyword + " before...");
         }
     }
@@ -274,5 +251,20 @@ public class InstructionParser {
         String keyword = findParts[1];
         return keyword;
     }
+
+    private void checkValidNumber(String[] parts, String errorMessage) throws ParsingException {
+        try {
+            Integer.parseInt(parts[1]);
+        } catch (NumberFormatException e) {
+            throw new ParsingException(errorMessage);
+        }
+    }
+
+    private void checkLengthMoreThanEqualTwo(String[] parts, String errorMessage) throws ParsingException {
+        if (parts.length < 2) {
+            throw new ParsingException(errorMessage);
+        }
+    }
+
 }
 // NOTE: Exceptions are checked twice, once in parseCommand and once in parse___Args()
